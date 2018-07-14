f824b888fc7bd1f1288ca402dc3ccf96da978904
Siketyan <siketyan@sikeserver.com>
2018-07-14 23:45:41 +0900

    Add /users/me endpoint

--- src/main/java/ga/local_connect/api/http/Endpoints.java
+++ src/main/java/ga/local_connect/api/http/Endpoints.java
@@ -6,13 +6,19 @@ import ga.local_connect.api.enumeration.EndpointCategory;
 import ga.local_connect.api.enumeration.HttpMethodType;
 import ga.local_connect.api.exception.LocalConnectException;
 import ga.local_connect.api.object.Session;
+import ga.local_connect.api.object.User;
 import org.eclipse.jetty.server.Request;
 
 import java.sql.SQLException;
 
 class Endpoints {
-    @Endpoint(method = HttpMethodType.GET, category = EndpointCategory.SESSIONS, name = "now")
-    public static Session showSession(Request req) throws SQLException, LocalConnectException {
+    @Endpoint(method = HttpMethodType.GET, category = EndpointCategory.SESSIONS, name = "current")
+    public static Session getCurrentSession(Request req) throws SQLException, LocalConnectException {
         return API.getSession(req.getHeader("X-LocalConnect-Session"));
+    }
+
+    @Endpoint(method = HttpMethodType.GET, category = EndpointCategory.USERS, name = "me")
+    public static User getCurrentUser(Request req) throws SQLException, LocalConnectException {
+        return getCurrentSession(req).getUser();
     }
 }
